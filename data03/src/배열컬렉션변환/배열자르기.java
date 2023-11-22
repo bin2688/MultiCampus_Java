722ccf2ed6f4a6d66462548ed53a42d127ef45e9
IMYOUNGBIN <bin2688@naver.com>
2023-11-22 17:03:32 +0900

    프로그래머스입문

---
+++ data03/src/배열컬렉션변환/배열자르기.java
@@ -0,0 +1,32 @@
+package 배열컬렉션변환;
+
+
+
+import 배열기본.Print;
+
+public class 배열자르기 {
+
+	public static void main(String[] args) {
+		Solution2 s = new Solution2();
+		int[] numbers = {1,2,3,4,5};
+		int num1 = 1;
+		int num2 = 3;
+		int[] result = s.solution(numbers, num1, num2);
+		Print.arrayPrint(result);
+	}
+}
+
+class Solution2 {
+	public int[] solution(int[] numbers, int num1, int num2) { //배열 나누어지는 수
+		//int[] answer = Arrays.copyOfRange(numbers, num1, num2 + 1);
+		
+		int[] answer = new int[num2 - num1 + 1];
+		int index = 0;
+		for (int i = num1; i < num2 +1 ; i++) {
+			answer[index] = numbers[i];
+			index++;
+		}
+    	return answer;
+	}
+}
+
