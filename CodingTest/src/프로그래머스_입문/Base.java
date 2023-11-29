89a2c2fff0e47fee528c0bd993e5b449668f75af
IMYOUNGBIN <bin2688@naver.com>
2023-11-29 18:12:16 +0900

    프로그래머스입문

---
+++ CodingTest/src/프로그래머스_입문/Base.java
@@ -0,0 +1,19 @@
+package 프로그래머스_입문;
+
+import java.util.Arrays;
+
+public class Base {
+
+	public static void main(String[] args) {
+		Solution0 s = new Solution0();
+		int[] result = s.solution(0);
+		System.out.println(Arrays.toString(result));
+	}
+}
+
+class Solution0 {
+	public int[] solution(int x) {
+		int[] answer = null;
+		return answer;
+	}
+}
