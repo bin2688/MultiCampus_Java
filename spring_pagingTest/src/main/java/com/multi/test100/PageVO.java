cbabf5d037aac4126c808e4a33190cf30e48367f
IMYOUNGBIN <bin2688@naver.com>
2023-12-07 13:33:08 +0900

    paging

---
+++ spring_pagingTest/src/main/java/com/multi/test100/PageVO.java
@@ -0,0 +1,27 @@
+package com.multi.test100;
+
+public class PageVO {
+	private int start;
+	private int end;
+	
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	
+	@Override
+	public String toString() {
+		return "PageVO [start=" + start + ", end=" + end + "]";
+	}
+	
+	
+	
+}
